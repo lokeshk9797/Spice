e4137430f136a5bdf0e92a77337499e87d687770
Lokesh Kashyap <lokeshkashyap858@gmail.com>
2019-10-16 15:15:59 +0530

    Changed Endpoint to include areas

--- Spice/Startup.cs
+++ Spice/Startup.cs
@@ -62,7 +62,7 @@ namespace Spice
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{areas=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
