849ec7c68369a2b7705a19f07e7c46fba2d34a02
lokesh.kashyap <lokesh.kashyap@globallogic.com>
2019-10-30 11:52:54 +0530

    Subcategory Migration Done Subcategory Controller Added SubCategory ViewModelAdded

---
+++ Spice/Models/ViewModel/SubCategoryAndCategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models.ViewModel
+{
+    public class SubCategoryAndCategoryViewModel
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public SubCategory NewSubCategory { get; set; }
+        public List<string> SubCategories { get; set; }
+        public string StatusMessage { get; set; }
+    }
+}
