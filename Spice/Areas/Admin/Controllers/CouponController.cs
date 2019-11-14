6a9f082ed426528e5964a10419f6ee6f02d2fc3a
Lokesh Kashyap <lokeshkashyap858@gmail.com>
2019-11-14 15:05:35 +0530

    Coupon Functionality Completed

--- Spice/Areas/Admin/Controllers/CouponController.cs
+++ Spice/Areas/Admin/Controllers/CouponController.cs
@@ -18,22 +18,27 @@ namespace Spice.Areas.Admin.Controllers
         {
             _db = db;
         }
+
+        //GET - INDEX
         public async Task< IActionResult> Index()
         {
             return View(await _db.Coupon.ToListAsync());
         }
 
+        //GET - CREATE
         public IActionResult Create()
         {
             return View();
         }
+
+        //POST - CREATE
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create (Coupon coupon)
         {
             if(!ModelState.IsValid)
             {
-
+                return NotFound();
             }
             var files = HttpContext.Request.Form.Files;
             if(files.Count>0)
@@ -55,5 +60,109 @@ namespace Spice.Areas.Admin.Controllers
             await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+        //GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await _db.Coupon.FindAsync(id);
+            if(coupon==null)
+            {
+                return NotFound();
+            }
+            return View(coupon);
+
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,Coupon coupon)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(coupon);
+            }
+            var couponFromDB = await _db.Coupon.FindAsync(id);
+            var files = HttpContext.Request.Form.Files;
+            if (files.Count > 0)
+            {
+                byte[] picture = null;
+                using (var fs1 = files[0].OpenReadStream())
+                {
+                    using (var ms1 = new MemoryStream())
+                    {
+                        fs1.CopyTo(ms1);
+                        picture = ms1.ToArray();
+                    }
+                }
+
+                coupon.Picture = picture;
+            }
+
+            couponFromDB.Name = coupon.Name;
+            couponFromDB.Discount = coupon.Discount;
+            couponFromDB.CouponType = coupon.CouponType;
+            couponFromDB.MinimumAmount = coupon.MinimumAmount;
+            couponFromDB.IsActive = coupon.IsActive;
+            couponFromDB.Picture = coupon.Picture;
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //GET - DETAILS
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await _db.Coupon.FindAsync(id);
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+            return View(coupon);
+
+        }
+        //GET - DELETE
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await _db.Coupon.FindAsync(id);
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+            return View(coupon);
+
+        }
+
+        //POST - DELETE
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var coupon = await _db.Coupon.SingleOrDefaultAsync(s => s.Id == id);
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+            _db.Coupon.Remove(coupon);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
     }
 }
--- Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Spice.Data;
 using Spice.Models;
+using Spice.Models.ViewModel;
 using System.Diagnostics;
 
 namespace Spice.Controllers
@@ -9,14 +12,21 @@ namespace Spice.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
+            //IndexViewModel indexVM = new IndexViewModel()
+            //{
+            //    MenuItems = _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync();
+            //};
+
             return View();
         }
 
